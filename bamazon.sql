237f03ee7af9600d08c0f0a98e01426645febbe7
Matt Jennings <matt@mattjennings.net>
2019-08-10 15:16:21 -0700

    middle of creating seed file

--- bamazon.sql
+++ bamazon.sql
@@ -6,9 +6,97 @@ USE bamazon;
 
 CREATE TABLE products (
     item_id INTEGER NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (item_id),
     product_name VARCHAR(255) NOT NULL,
     department_name VARCHAR(255) NOT NULL,
     price FLOAT NOT NULL,
-    stock_quantity INTEGER NOT NULL
+    stock_quantity INTEGER NOT NULL,
+    PRIMARY KEY (item_id)
 );
+
+INSERT INTO products 
+VALUES (
+    null,
+    'Star Wars Tauntaun Sleeping Bag',
+    'Sports & Outdoors',
+    799.99,
+    92
+);
+
+
+
+
+INSERT INTO products 
+VALUES (
+    null,
+    'Gramatik - Street Bangerz Volume 2',
+    'CDs & Vinyl',
+    49.95,
+    9
+);
+INSERT INTO products 
+VALUES (
+    null,
+    'National Parks Exploration Series - Glacier National Park - Crown of the Continent',
+    'Movies & TV',
+    $5.49,
+    211
+);
+
+
+INSERT INTO products 
+VALUES (
+    null,
+    'here',
+    'here',
+    66,
+    66
+);
+INSERT INTO products 
+VALUES (
+    null,
+    'here',
+    'here',
+    66,
+    66
+);
+INSERT INTO products 
+VALUES (
+    null,
+    'here',
+    'here',
+    66,
+    66
+);
+INSERT INTO products 
+VALUES (
+    null,
+    'here',
+    'here',
+    66,
+    66
+);
+INSERT INTO products 
+VALUES (
+    null,
+    'here',
+    'here',
+    66,
+    66
+);
+INSERT INTO products 
+VALUES (
+    null,
+    'here',
+    'here',
+    66,
+    66
+);
+INSERT INTO products 
+VALUES (
+    null,
+    'here',
+    'here',
+    66,
+    66
+);
+
