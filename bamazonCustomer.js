d3bb8782eea7dbac39bf25ba867f36f3a36bf054
Matt Jennings <matt@mattjennings.net>
2019-08-13 19:47:51 -0700

    completed assignment, now adding comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+// Get mysql and insquirer modules
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+// Get mysql connection
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -9,6 +11,8 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
+// Function to show all items on running
+// node bamazonCustomer.js
 function showAllItems() {
     connection.connect(function(err) {
         if(err) throw err;
@@ -23,6 +27,7 @@ function showAllItems() {
     });
 }
 
+// Function call
 showAllItems();
 
 function start() {
@@ -40,27 +45,34 @@ function start() {
     ])
     .then(function(myProduct) {
 
-        var query1 = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE item_id = ?"; 
-        connection.query(query1, [ myProduct.idItemToBuy ], function(err, item) {
+        var query = "SELECT item_id, product_name, price, stock_quantity FROM products WHERE item_id = ?"; 
+        connection.query(query, [ myProduct.idItemToBuy ], function(err, item) {
             if(err) throw err;
 
             var totalCost = Number.parseFloat(item[0].price * parseInt(myProduct.quantityItemToBuy)).toFixed(2);
 
             if(myProduct.quantityItemToBuy <= item[0].stock_quantity) {
                 console.log(`You purchased ${myProduct.quantityItemToBuy} of ${item[0].product_name} at a total cost of $${totalCost} ($${item[0].price} per item).`);
+                updateProductQuantity(100, 1);
             }
             else {
                 console.log(' Insufficient quantity!');
             }
 
             connection.end();
+            
         });
 
         // Update products where item_id = myProduct.idItemToBuy
 
         // Create separate function that will take id and number and call function
         // inside of query
-        
+        function updateProductQuantity(quantity, item_id) {
+            var query = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
+            connection.query(query, [quantity, item_id], function(err, item) {
+                if(err) throw err;
+            });
+        }              
         
     });
 }
